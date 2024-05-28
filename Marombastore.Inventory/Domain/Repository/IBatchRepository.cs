a56090d971cd8ff90ee408d2e6004e5fff6943da
Vinicius Oliveira <eovinicius10@gmail.com>
2024-05-28 17:14:58 -0300

    fix: batch repository name

--- Marombastore.Inventory/Domain/Repository/IBatchRepository.cs
+++ Marombastore.Inventory/Domain/Repository/IBatchRepository.cs
@@ -2,7 +2,7 @@ using Marombastore.Core.Seedwork;
 using Marombastore.Inventory.Domain.Entity;
 
 namespace Marombastore.Inventory.Domain.Repository;
-public interface IInventoryBatchRepository
-: IRepository<InventoryBatch>
+public interface IBatchRepository
+: IRepository<Batch>
 { }
 
