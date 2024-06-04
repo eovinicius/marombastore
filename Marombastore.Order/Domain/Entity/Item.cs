0a15646ecc78bd6ecbed3472849f25c55bbc5d4f
Vinicius Oliveira <eovinicius10@gmail.com>
2024-06-04 17:08:02 -0300

    refactor: entity orderItem and item

--- Marombastore.Order/Domain/Entity/Item.cs
+++ Marombastore.Order/Domain/Entity/Item.cs
@@ -7,34 +7,10 @@ public class Item : AggregateRoot
     public string Name { get; private set; }
     public decimal Price { get; private set; }
     public string Description { get; private set; }
-    public bool Active { get; private set; }
-
-    public Item(string name, decimal price, string description, bool active)
+    public Item(string name, decimal price, string description)
     {
         Name = name;
         Price = price;
-        Description = description;
-        Active = active;
-    }
-
-    public void Activate()
-    {
-        if (!Active) Active = true;
-    }
-
-    public void Deactivate()
-    {
-        if (Active) Active = false;
-    }
-
-    public void ChangePrice(decimal price)
-    {
-        if (price <= 0) throw new Exception("Price must be greater than zero");
-        Price = price;
-    }
-
-    public void ChangeDescription(string description)
-    {
         Description = description;
     }
 }
--- Marombastore.Order/Domain/Entity/OrderItem.cs
+++ Marombastore.Order/Domain/Entity/OrderItem.cs
@@ -5,17 +5,21 @@ namespace Marombastore.Order.Domain.Entity;
 public class OrderItem : EntityBase
 {
     public Item Item { get; private set; }
-    public decimal Price { get; private set; }
+    public decimal UnitPrice { get; private set; }
+    public decimal Total { get; private set; }
     public int Quantity { get; private set; }
 
-    public OrderItem(Item item, decimal price, int quantity)
+    public OrderItem(Item item, decimal unitPrice, int quantity)
     {
         Item = item;
-        Price = price;
+        UnitPrice = unitPrice;
+        Total = GetTotal();
         Quantity = quantity;
+
+        Validate();
     }
 
-    public decimal GetTotal() => Item.Price * Quantity;
+    public decimal GetTotal() => UnitPrice * Quantity;
 
     private void Validate()
     {
